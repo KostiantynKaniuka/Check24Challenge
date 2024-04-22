463c07ccfe5ee96135d24bd4726baa1cce84b83e
heyhocombo <heyhocombo@gmail.com>
2024-04-22 12:21:28 +0200

    Initial Commit

---
+++ Check24Challenge/Check24ChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Check24ChallengeApp.swift
+//  Check24Challenge
+//
+//  Created by Kostiantyn Kaniuka on 22.04.2024.
+//
+
+import SwiftUI
+
+@main
+struct Check24ChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
